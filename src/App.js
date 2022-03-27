de281996ce108e033634130d7497a4ffe7aee571
Julien Anquetil <anquetil.jul@gmail.com>
2022-03-27 22:20:26 +0200

    moment + date

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import moment from 'moment';
+import 'moment/locale/fr'
+
 import { TextInput, Button, Box, Title, Paper, Text, Group } from '@mantine/core';
 import { useForm } from '@mantine/form';
 
+import './App.css';
+
 function App() {
+  const [todos, setTodos] = useState(
+    [
+      {
+        text: "Use it to create cards, dropdowns, modals and other components that require background",
+        date: Date.now()
+      }
+    ]
+  );
+
   const form = useForm({
     initialValues: {
       todo: '',
@@ -29,15 +44,21 @@ function App() {
       <Box sx={{ marginTop: 20}} max="auto" className="box">
         <Paper shadow="xl" radius="md" p="xl" sx={{ marginRight: 20}} className="card" withBorder>
           <Title order={3}>Todo</Title>
-          <Paper sx={{ marginTop: 10}} shadow="md" radius="lg" p="md" withBorder>
-            <Group position="right">
-              <Button type="submit">Submit</Button>
-            </Group>
-            <Text>
-              Use it to create cards, dropdowns, modals and other components that require background
-              with shadow
-            </Text>
-          </Paper>
+          {
+            todos.map(todo => (
+              <Paper sx={{ marginTop: 10}} shadow="md" radius="lg" p="md" withBorder>
+                <Group position="right" >
+                  <Text color="gray" size="sm">
+                    Créé le : {moment(todo.date).format('lll')}
+                  </Text>
+                  <Button type="submit">Submit</Button>
+                </Group>
+                <Text>
+                  {todo.text}
+                </Text>
+              </Paper>
+            ))
+          }
         </Paper>
         <Paper shadow="xl" radius="md" p="xl" className="card" withBorder >
           <Title order={3}>Archives</Title>
