ce04331ca044f1b728e9a43eec57152d59bd9477
Julien Anquetil <anquetil.jul@gmail.com>
2022-03-27 22:02:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { TextInput, Button, Box, Title, Paper, Text, Group } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 function App() {
+  const form = useForm({
+    initialValues: {
+      todo: '',
+    },
+  });
+
+  const handleSubmit = (values) => console.log(values);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title order={1}>Todolist Siu et Ju</Title>
+      <Box sx={{ marginTop: 20 }} mx="auto">
+        <form onSubmit={form.onSubmit(handleSubmit)} className="box">
+          <TextInput
+            sx={{ width: '50vw' }}
+            required
+            placeholder="Nouveau todo"
+            {...form.getInputProps('todo')}
+          />
+
+          <Button type="submit">Ajouter</Button>
+        </form>
+      </Box>
+      <Box sx={{ marginTop: 20}} max="auto" className="box">
+        <Paper shadow="xl" radius="md" p="xl" sx={{ marginRight: 20}} className="card" withBorder>
+          <Title order={3}>Todo</Title>
+          <Paper sx={{ marginTop: 10}} shadow="md" radius="lg" p="md" withBorder>
+            <Group position="right">
+              <Button type="submit">Submit</Button>
+            </Group>
+            <Text>
+              Use it to create cards, dropdowns, modals and other components that require background
+              with shadow
+            </Text>
+          </Paper>
+        </Paper>
+        <Paper shadow="xl" radius="md" p="xl" className="card" withBorder >
+          <Title order={3}>Archives</Title>
+          <Paper sx={{ marginTop: 10}} shadow="md" radius="lg" p="md" withBorder>
+            <Text>Paper is the most basic ui component</Text>
+            <Text>
+              Use it to create cards, dropdowns, modals and other components that require background
+              with shadow
+            </Text>
+          </Paper>
+        </Paper>
+      </Box>
     </div>
   );
 }
